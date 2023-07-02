41f0242ccd75a109a9788c566ffc28586301ca20
Василий Толстов <v.tolstov@a3f.ru>
2023-07-02 15:14:59 +0300

    tasks 1 dn 2

---
+++ 1-twoSum.go
@@ -0,0 +1,15 @@
+package twosum
+
+func twoSum(nums []int, target int) []int {
+	var indexes []int
+
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < (len(nums) - i); j++ {
+			if nums[i]+nums[j] == target {
+				indexes = append(indexes, i, i)
+			}
+		}
+	}
+
+	return indexes
+}

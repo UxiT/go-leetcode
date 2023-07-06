822a7601205f0d465027d4e31632c0a55c814460
Василий Толстов <v.tolstov@a3f.ru>
2023-07-06 18:15:14 +0300

    t13 Roman to int

---
+++ 13_romanToInt.go
@@ -0,0 +1,29 @@
+package algos
+
+// Assume that largest number is always on the left
+func romanToInt(s string) int {
+	sum := 0
+
+	symMap := map[byte]int{
+		'I': 1,
+		'V': 5,
+		'X': 10,
+		'L': 50,
+		'C': 100,
+		'D': 500,
+		'M': 1000,
+	}
+
+	for i := range s {
+		if (i+1) < len(s) && symMap[s[i+1]] > symMap[s[i]] {
+			sum -= symMap[s[i]]
+		} else {
+			sum += symMap[s[i]]
+		}
+	}
+
+	return sum
+}
+
+// Runtime 88.45%
+// Memory 88.72%

c73f7b4f20319b10bfd6a03de05ec4c1127c3a3e
Василий Толстов <v.tolstov@a3f.ru>
2023-07-11 14:59:20 +0300

    20_Parenthesis

---
+++ 20_valid_parentheses.go
@@ -0,0 +1,24 @@
+package algos
+
+func isValid(s string) bool {
+	var stack []rune
+	mapping := map[rune]rune{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+
+	for _, char := range s {
+		if char == '(' || char == '[' || char == '{' {
+			stack = append(stack, char)
+		} else {
+			if len(stack) == 0 || stack[len(stack)-1] != mapping[char] {
+				return false
+			}
+
+			stack = stack[:len(stack)-1]
+		}
+	}
+
+	return len(stack) == 0
+}

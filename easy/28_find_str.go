4f18fa8ef4a32b0d9053a63537dc57b95f12a5b6
Василий Толстов <v.tolstov@appcntr.ru>
2024-01-27 02:09:01 +0300

    28 solved

---
+++ easy/28_find_str.go
@@ -0,0 +1,22 @@
+package algos
+
+func strStr(haystack string, needle string) int {
+	haystackLen := len(haystack)
+	needleLen := len(needle)
+
+	if needleLen > haystackLen {
+		return -1
+	}
+
+	if needle == haystack {
+		return 0
+	}
+
+	for i := 0; i < (haystackLen - needleLen + 1); i++ {
+		if haystack[i:i+needleLen] == needle {
+			return i
+		}
+	}
+
+	return -1
+}

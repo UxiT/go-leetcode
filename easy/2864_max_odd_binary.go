e6d30e3a673073c332f66b06828bb55794da879b
Василий Толстов <v.tolstov@appcntr.ru>
2024-02-21 23:38:43 +0300

    2864

---
+++ easy/2864_max_odd_binary.go
@@ -0,0 +1,18 @@
+package algos
+
+import "strings"
+
+func maximumOddBinaryNumber(s string) string {
+	zeroes := 0
+	ones := 0
+
+	for _, c := range s {
+		if c == '1' {
+			ones++
+		} else {
+			zeroes++
+		}
+	}
+
+	return strings.Repeat("1", ones-1) + strings.Repeat("0", zeroes) + "1"
+}
